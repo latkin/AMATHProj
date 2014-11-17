8fead83141a394cc33d03a12d1b663da3627ef31
latkin <latkin@users.noreply.github.com>
2014-11-16 19:55:09 -0800

    2-flip search, and continuous scanning function

--- src/driver/Program.fs
+++ src/driver/Program.fs
@@ -1,7 +1,6 @@
 ﻿open AMATHProj.Lib
 
-
-let numVtx = 25
+let numVtx = 32
 let numColors = 2
 let cliqueSize = 5
 
@@ -10,8 +9,4 @@ let g = Graph.random numVtx numColors
 let mutable numCliques = g |> Graph.numCliques cliqueSize
 printfn "Starting: %d" numCliques
 
-let mutable newNum = Algos.makeFirstHelpful1Flip numCliques cliqueSize g
-while newNum < numCliques do
-    printfn "New lowest: %d" newNum
-    numCliques <- newNum
-    newNum <- Algos.makeFirstHelpful1Flip numCliques cliqueSize g
+Algos.continuousScan numCliques cliqueSize g
--- src/graph/algos.fs
+++ src/graph/algos.fs
@@ -20,15 +20,62 @@ module Algos =
 
     let makeFirstHelpful1Flip cliqueCount cliqueSize (g:G) =
         let mutable lowestCliqueCount = cliqueCount
-        let mutable i = 0
-        while i < g.Length do
-            Graph.flipEdge i g
-            let newCliqueCount = Graph.Parallel.numCliques cliqueSize g
+        let mutable i1 = 0
+        while i1 < g.Length do
+            Graph.flipEdge i1 g
+            let newCliqueCount = Graph.numCliques cliqueSize g
             if newCliqueCount < lowestCliqueCount then
                 lowestCliqueCount <- newCliqueCount
-                i <- Int32.MaxValue
+                i1 <- Int32.MaxValue
             else
-                Graph.flipEdge i g
-                i <- i + 1
+                Graph.flipEdge i1 g
+                i1 <- i1 + 1
 
         lowestCliqueCount
+
+
+    let makeFirstHelpful2Flip cliqueCount cliqueSize (g:G) =
+        let mutable lowestCliqueCount = cliqueCount
+        let mutable i1 = 0
+        let mutable i2 = 0
+        let mutable go = true
+
+        while i1 < g.Length - 1 && go do
+            Graph.flipEdge i1 g
+            i2 <- i1 + 1
+            while i2 < g.Length && go do
+                Graph.flipEdge i2 g
+                let newCliqueCount = Graph.numCliques cliqueSize g
+                if newCliqueCount < lowestCliqueCount then
+                    lowestCliqueCount <- newCliqueCount
+                    go <- false
+                else
+                    Graph.flipEdge i2 g
+                    i2 <- i2 + 1
+            if go then
+                Graph.flipEdge i1 g
+                i1 <- i1 + 1
+
+        lowestCliqueCount
+    
+    let rec private continuousImpl level cliqueCount cliqueSize (g:G) =
+        let newCliqueCount = 
+            match level with
+            | 1 -> makeFirstHelpful1Flip cliqueCount cliqueSize g
+            | 2 -> makeFirstHelpful2Flip cliqueCount cliqueSize g
+            | _ -> cliqueCount
+
+        if newCliqueCount < cliqueCount then
+            printfn "New best (lvl %d): %d" level newCliqueCount
+            if newCliqueCount = 0 then
+                printfn "%s" (Graph.toString Raw g)
+                0
+            else
+                continuousImpl 1 newCliqueCount cliqueSize g
+        else
+            if level = 3 then cliqueCount else
+            printfn "Bumping level"
+            continuousImpl (level+1) cliqueCount cliqueSize g
+
+    let continuousScan cliqueCount cliqueSize (g:G) =
+        continuousImpl 1 cliqueCount cliqueSize g
